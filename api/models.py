dce9ff2d8287c5b36ae6e05a79928faf6b6ac53b
Koh Ishibashi <trendy8455@gmail.com>
2021-01-17 23:52:25 +0900

    add migration file and admin panel setting

--- api/models.py
+++ api/models.py
@@ -7,3 +7,17 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+class Account(models.Model):
+    name = models.CharField(max_length=255)
+    mail = models.EmailField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    body = models.CharField(max_length=255)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.body
